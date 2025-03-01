ccbd46f7b08f1752ebd428029b7d5a428ba9e16f
Shinobu Oshino <lucas.lucena@ccc.ufcg.edu.br>
2025-03-01 09:53:53 -0300

    :sparkles: feat: Criando models para Movie e Review

--- app_rotten_potatoes/models.py
+++ app_rotten_potatoes/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    genre = models.CharField(max_length=255)
+    year = models.IntegerField()
+    director = models.CharField(max_length=255)
+    rating = models.FloatField()
+
+    def __str__(self):
+        return self.title
+
+class Review(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.movie.title} - {self.rating} - {self.body}'
