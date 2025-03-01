62ce50cd9d46b77c569b8ec8ff99b87328e8a7aa
Shinobu Oshino <lucas.lucena@ccc.ufcg.edu.br>
2025-03-01 20:30:14 -0300

    :bulb: docs: Comentários sobre banco de dados db.sqlite3

--- project_rotten_potatoes/settings.py
+++ project_rotten_potatoes/settings.py
@@ -75,6 +75,15 @@ WSGI_APPLICATION = 'project_rotten_potatoes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# Ver README.md para mais informações sobre o banco de dados
+# Em # Rodando o Server com Metodos Legado
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.db.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -85,6 +94,7 @@ DATABASES = {
         'PORT': 5432,
     }
 }
+
 
 
 # Password validation
